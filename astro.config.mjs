ffcba4b8eccd74cb58ec36c9858163ba0bb76874
Miro Ostafiński <ostafinskim@gmail.com>
2023-06-25 20:19:05 +0100

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "@todo",
+  site: "https://ostafinski.cc",
   integrations: [
     tailwind(),
     image({
